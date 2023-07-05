e3326d4010002eefabecbe20bd028be8fa879c8a
Lindsay Platt <lrplatt@wisc.edu>
2023-07-05 12:42:32 -0500

    add more unit conversions for calcium data

--- 3_harmonize/src/clean_calcium_data.R
+++ 3_harmonize/src/clean_calcium_data.R
@@ -13,8 +13,40 @@
 clean_calcium_data <- function(wqp_data){
 
   # Clean calcium data
-  wqp_data_out <- wqp_data
-  # TODO: come up with calcium cleaning steps
+  wqp_data_harmonized_units <- wqp_data %>% 
+    # Convert values from various concentrations to mg/L
+    mutate(conversion_multiplier = case_when(
+      is.na(ResultMeasure.MeasureUnitCode) ~ NA_integer_, # This will effectively, turn the result into an NA value
+      tolower(ResultMeasure.MeasureUnitCode) == "mg/l" ~ 1,
+      tolower(ResultMeasure.MeasureUnitCode) == "ug/l" ~ (1/1000),
+      tolower(ResultMeasure.MeasureUnitCode) == "mmol/l" ~ 35.453, # THIS IS DIFFERENT
+      tolower(ResultMeasure.MeasureUnitCode) == "mg/g" ~ 1000,
+      tolower(ResultMeasure.MeasureUnitCode) == "mg/kg" ~ 1,
+      tolower(ResultMeasure.MeasureUnitCode) == "ppm" ~ 1,
+      # TODO: what about `mg/l CaCO3**`, `mg/l CaCO3`, `ueq/L`, and `%`
+      TRUE ~ NA_integer_
+    )) %>% 
+    mutate(ResultMeasureValue = ResultMeasureValue * conversion_multiplier) %>% 
+    # Now update units for those unit codes we converted, keep original value
+    mutate(ResultMeasure.MeasureUnitCode_original = ResultMeasure.MeasureUnitCode) %>% 
+    mutate(ResultMeasure.MeasureUnitCode = case_when(
+      is.na(ResultMeasure.MeasureUnitCode) ~ as.character(NA),
+      tolower(ResultMeasure.MeasureUnitCode) %in% c(
+        "mg/l", "ug/l", 
+        "mmol/l",
+        "mg/g", "mg/kg", "ppm"
+      ) ~ "mg/l",
+      TRUE ~ ResultMeasure.MeasureUnitCode
+    )) %>% 
+    select(-conversion_multiplier)
+  
+  # Now filter out missing values (some became a missing value if their unit code was unknown)
+  wqp_data_out <- wqp_data_harmonized_units %>% 
+    filter(!is.na(ResultMeasureValue)) %>% 
+    harmonize_output_columns() # Have columns match output of all other characteristics
+  
+  message(sprintf('`clean_calcium_data()` units harmonization resulted in %s records being dropped',
+                  nrow(wqp_data_harmonized_units)-nrow(wqp_data_out)))
   
   return(wqp_data_out)
   
